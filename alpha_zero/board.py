cf6f7eec113e6eb74b0a7921b8f81bd2454ee258
lihongxun945 <lihongxun945@gmail.com>
2023-05-29 23:37:25 +0800

    feat: 增加 Net

--- alpha_zero/board.py
+++ alpha_zero/board.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import copy
+import numpy as np
 '''
 帮我用python3写一个五子棋的Board类，实现以下功能：
 1. __init__: 初始化方法用来初始化棋盘，可以指定棋盘大小，默认为15，可以指定先手颜色，默认为黑子先手
@@ -12,6 +13,12 @@ import copy
 8. get_board_string: 获取当前棋盘的字符串表示，把棋盘上所有棋子拼成一个字符串来表示
 
 注意，所有位置都用一个整数表示，从0开始，从左到右，从上到下依次编号, 所有棋子都是用一个整数表示，1表示黑子，-1表示白子，0表示空位
+'''
+
+'''
+为了用Net模型，需要改造一下Board类，增加一个get_data方法，这个方法会返回一个训练数据 [x, y]
+其中 x 是 n*n*17 的张量，其中8个平面表示当前棋盘上黑子最近8次落子后的状态，8个平面表示当前棋盘上白子的最近8次落子后的状态，最后一个平面表示当前轮到哪一方落子，如果是黑方落子，则这个平面全部为1，否则为-1
+其中 y 是一个二维向量 [v, pi]，v 表示当前棋盘的胜负情况，1表示黑方胜，-1表示白方胜，0表示未分出胜负，pi 是一个n*n的向量，表示黑方落子的概率，其中非法位置的概率为0
 '''
 
 # 以下是Chatgpt4.0 写的代码, 并做了少量修改
@@ -100,3 +107,46 @@ class Board:
         new_board.board = copy.deepcopy(self.board)
         new_board.history = copy.deepcopy(self.history)
         return new_board
+
+    def position_to_coordinate(self, position):
+        """
+        将整数位置转换为二维坐标
+        """
+        row = position // self.size
+        col = position % self.size
+        return row, col
+
+    def get_data(self):
+        x = np.zeros((self.size, self.size, 17), dtype=np.int8)
+
+        # 填充棋盘状态
+        history_len = len(self.history)
+        for i in range(16):
+            if i < history_len:
+                # 获取历史状态
+                state = np.zeros((self.size, self.size), dtype=np.int8)
+                # 从history中获取每一次的落子，然后把它转换成棋盘状态
+                for j in range(history_len - i):
+                    position, color = self.history[j]
+                    row, col = self.position_to_coordinate(position)
+                    state[row, col] = color
+
+                # 将每一次落子后的棋盘状态标记到x的前16个平面上
+                x[:, :, 15 - i] = state
+
+        # 最后一个平面表示当前轮到哪一方落子
+        x[:, :, -1] = self.current_color  # 全部的值都是当前角色
+
+        v = self.get_winner()  # 胜负情况
+
+        # 创建一个全零向量表示落子概率
+        pi = np.zeros(self.size * self.size)
+        if len(self.history) > 0:
+            # 最后一步落子位置的概率设为1，其他设为0
+            last_move = self.history[-1][0]
+            pi[last_move] = 1
+        
+        y = [v, pi]
+
+        return x, y
+
---
+++ alpha_zero/net.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+'''
+帮我用tensorflow写一个Net类，实现方法参考Alpha Zero的论文，这个类中会构造一个model模型，这个模型需要接收的输入维度是 n * n * 17，有两个输出层，其中一个输出落子的概率分布，是一个n*n的一位向量，每一个值表示当前位置的落子概率。另一个输出获胜的角色，实现以下方法：
+- __init__: 初始化方法，需要传入棋盘的大小，默认为15, 在这个方法中构造网络模型
+- predict: 预测方法，传入一个棋盘状态 n*n*17，返回两个值，第一个值是落子概率分布，第二个值是获胜的角色
+- train: 用训练数据集进行训练
+- save: 保存模型checkpoint
+注意其中的n是棋盘的大小，输入维度是17, 17个平面，其中8个平面表示当前玩家的棋子分布，8个平面表示对手玩家的棋子分布，最后一个平面表示当前玩家的颜色，1表示黑棋，-1表示白棋
+网络模型就用tensorflow内置的Resnet50
+'''
+
+import tensorflow as tf
+from tensorflow.keras import Model
+from tensorflow.keras.layers import Input, Conv2D, BatchNormalization, Activation, Add, Flatten, Dense
+from tensorflow.keras.applications.resnet50 import ResNet50
+
+class Net:
+    def __init__(self, board_size=15):
+        self.board_size = board_size
+        self.model = self.build_model()
+
+    def build_model(self):
+        base_model = ResNet50(weights=None, include_top=False, input_shape=(self.board_size, self.board_size, 17))
+        
+        x = base_model.output
+        x = Conv2D(filters=2, kernel_size=(1, 1))(x)
+        x = BatchNormalization()(x)
+        x = Activation('relu')(x)
+
+        # Policy Head
+        policy_head = Conv2D(filters=2, kernel_size=(1, 1))(x)
+        policy_head = BatchNormalization()(policy_head)
+        policy_head = Activation('relu')(policy_head)
+        policy_head = Flatten()(policy_head)
+        policy_head = Dense(self.board_size * self.board_size, activation='softmax', name='policy')(policy_head)
+
+        # Value Head
+        value_head = Conv2D(filters=1, kernel_size=(1, 1))(x)
+        value_head = BatchNormalization()(value_head)
+        value_head = Activation('relu')(value_head)
+        value_head = Flatten()(value_head)
+        value_head = Dense(256, activation='relu')(value_head)
+        value_head = Dense(1, activation='tanh', name='value')(value_head)
+
+        model = Model(inputs=base_model.input, outputs=[policy_head, value_head])
+        model.compile(loss={'policy': 'categorical_crossentropy', 'value': 'mean_squared_error'},
+                      optimizer='adam', metrics=['accuracy'])
+
+        return model
+
+    def predict(self, board_state):
+        board_state = board_state.reshape(-1, self.board_size, self.board_size, 17)
+        policy, value = self.model.predict(board_state)
+        return policy[0], value[0][0]
+
+    def train(self, x_train, y_train, batch_size=256, epochs=10):
+        self.model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs)
+
+    def save(self, path):
+        self.model.save_weights(path)
