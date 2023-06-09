66feff2289dac760fe98ed6ef867862f54b39fc4
hongxun.lhx <lihongxun945@gmail.com>
2023-06-09 17:28:06 +0800

    feat: put players in one file

--- alpha_zero/players.py
+++ alpha_zero/players.py
@@ -1,11 +1,22 @@
 # -*- coding: utf-8 -*-
+from alpha_zero.pure_mcts import PureMCTS
+from alpha_zero.mcts import MCTS
+from alpha_zero.net import Net
+
+class MCTSPlayer:
+  def __init__(self, board, simulation_num=400):
+    net = Net(board.size)
+    net.load('checkpoint/best_checkpoint.h5')
+    self.mcts = MCTS(board=board, net=net, simulation_num=simulation_num, self_play=False)
+
+  def move(self):
+    return self.mcts.move(temp=0)
 '''
 帮我用python3写一个类HumanPlayer，作用是让人类玩家下棋，实现以下方法：
 1. __init__: 初始化方法，需要传入board
 2. move: 从控制台读取玩家的输入，返回玩家的落子位置，输入的是棋盘的横纵坐标，用空格或者逗号分隔，返回的是一个整数表示落子位置.注意要用 get_valid_moves 验证玩家落子是否合法
 board 中有一个方法 get_size 可以返回棋盘的大小
 '''
-
 
 # 以下是Chatgpt4.0 写的代码
 class HumanPlayer:
@@ -26,3 +37,16 @@ class HumanPlayer:
         return position
       else:
         print("Invalid move. Please enter a valid move.")
+
+
+'''
+帮我用python3写一个类PureMCTSPlayer, 使用上面写的PureMCTS实现一个简单的AI，实现以下方法：
+1. __init__: 初始化方法，需要传入board
+2. move: 调用PureMCTS返回玩家的落子位置，返回的是一个整数表示落子位置
+'''
+class PureMCTSPlayer:
+  def __init__(self, board, simulation_num=1000):
+    self.mcts = PureMCTS(board, simulation_num)
+
+  def move(self):
+    return self.mcts.move(None, True)
--- start_arena.py
+++ start_arena.py
@@ -1,25 +1,14 @@
 # -*- coding: utf-8 -*-
 from alpha_zero.arena import Arena
-from alpha_zero.human_player import HumanPlayer
+from alpha_zero.players import HumanPlayer, MCTSPlayer
 from alpha_zero.board import Board
-from alpha_zero.mcts import MCTS
-from alpha_zero.net import Net
 
-board_size=8
+board_size=11
 silumation_num=400
 
 board = Board(size=board_size)
 
-class MCTSPlayer:
-  def __init__(self):
-    net = Net(board_size)
-    net.load('checkpoint/best_checkpoint.h5')
-    self.mcts = MCTS(board=board, net=net, simulation_num=silumation_num, self_play=False)
-
-  def move(self):
-    return self.mcts.move(temp=0)
-
-ai1 = MCTSPlayer()
+ai1 = MCTSPlayer(board=board, simulation_num=silumation_num)
 ai2 = HumanPlayer(board)
 
 arena = Arena(board, ai1, ai2)
